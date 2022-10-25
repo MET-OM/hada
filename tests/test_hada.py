7c475ddb4b656805d76243ed180ea338c54c143f
Gaute Hope <gaute.hope@met.no>
2022-10-25 15:00:05 +0200

    rm hardcoded path

--- tests/test_hada.py
+++ tests/test_hada.py
@@ -7,6 +7,8 @@ def test_run(sourcetoml, tmpdir, runner):
         r = runner.invoke(hada, ['--sources', str(sourcetoml), '--output', td / 'test.nc' ])
         assert r.exit_code == 0
 
-def test_custom_bbox(sourcetoml, runner):
-    r = runner.invoke(hada, ['--sources', str(sourcetoml), '--output', '/tmp/test.nc', '--bbox', '6,7,67,68'])
-    assert r.exit_code == 0
+def test_custom_bbox(sourcetoml, tmpdir, runner):
+    with runner.isolated_filesystem(temp_dir=tmpdir) as td:
+        td = Path(td)
+        r = runner.invoke(hada, ['--sources', str(sourcetoml), '--output', td / 'test.nc', '--bbox', '6,7,67,68'])
+        assert r.exit_code == 0
