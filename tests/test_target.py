e6d849f19325039372fbec7e5fdbbcd7db4612c0
Gaute Hope <gaute.hope@met.no>
2022-12-06 22:16:47 +0100

    test: target upd

--- tests/test_target.py
+++ tests/test_target.py
@@ -13,7 +13,7 @@ def test_calculate_grid(tmpdir):
     d = Dataset(
         "norkyst",
         "https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be",
-        ['x_wind'])
+        'X', 'Y', ['x_wind'])
     t = Target(5, 10, 55, 60, 100, 100, tmpdir)
 
     tx, ty, _ = d.__calculate_grid__(t)
