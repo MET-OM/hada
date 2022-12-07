d401c68bc730c24ab958548c5b19a71651abd988
Gaute Hope <gaute.hope@met.no>
2022-12-07 11:07:06 +0100

    compare regridded norkyst

---
+++ tests/test_source_regrid.py
@@ -0,0 +1,40 @@
+import pytest
+from hada.sources import *
+from hada.target import Target
+
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+
+
+def test_norkyst_transform_points(tmpdir):
+    d = Dataset(
+        "norkyst",
+        "https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be",
+        'X', 'Y', ['x_wind'])
+    assert d.ds.rio.crs is not None
+
+    ## Norkyst EPSG: 8901
+
+    v = d.ds['Uwind'].sel(time="2022-11-06T02:00:00")
+    print(v)
+
+    ## Try to get some values and check if they end up where they should.
+    t = Target.from_lonlat(5, 10, 55, 60, 100, 100, tmpdir)
+
+    vo = d.regrid(d.ds['Uwind'], t, "2022-11-06T02:00:00", "2022-11-06T02:00:00")
+    print(vo)
+
+    ncrs = ccrs.Stereographic(true_scale_latitude=60,
+                              central_latitude=90,
+                              central_longitude=70,
+                              false_easting=3192800,
+                              false_northing=1784000)
+
+    ax = plt.axes(projection=ccrs.Mercator())
+    v.plot(transform=ncrs)
+
+    plt.figure()
+    ax = plt.axes(projection=ccrs.Mercator())
+    vo.plot(transform=t.cartopy_crs)
+
+    plt.show()
