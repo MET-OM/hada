71a0accc114f6556adc8e353aad6c8eb28f819f5
Gaute Hope <gaute.hope@met.no>
2023-02-09 21:49:21 +0100

    era5: test kdtree interp

--- tests/test_nearest_interpolation.py
+++ tests/test_nearest_interpolation.py
@@ -1,3 +1,4 @@
+import pytest, os
 from pyproj import CRS
 from pytest import approx
 from hada.sources import *
@@ -125,5 +126,42 @@ def test_mywave_nans(tmpdir):
     np.testing.assert_allclose(tyi, tcyi, atol=6)
 
     vo = d.regrid(d.ds['hs'], t, pd.to_datetime("2019-11-06T02:00:00"), always_nearest=True)
+    assert not np.isnan(vo.values).any()
+
+@pytest.mark.skipif(
+    not os.path.exists('/lustre/storeB/project/fou/om/ERA/ERA5/atm'),
+    reason="dataset not accessible, skipping dependent tests")
+def test_era5_index(tmpdir):
+    d = Dataset(
+        "era5",
+        "/lustre/storeB/project/fou/om/ERA/ERA5/atm/era5_sst_CDS_202205.nc",
+        'longitude',
+        'latitude', ['sst'],
+        proj4='+proj=latlong')
+
+    t = Target.from_gridfile('projects/Svalbard_3km_Grid_EPSG3575_Kyst.csv',
+                             tmpdir)
+
+    tcx, tcy, ibc = d.__calculate_grid__(t)
+    assert ibc.ravel().all()
+
+    tx, ty, tx_i, ty_i, ib = d.__interpolate_nearest_valid_grid__(t, 'sst')
+    assert ib.ravel().all()
+
+    np.testing.assert_array_equal(tx, d.ds.X.values[tx_i])
+    np.testing.assert_array_equal(ty, d.ds.Y.values[ty_i])
+
+    # find index of tx and ty
+    txi, tyi = d.__map_to_index__(tx, ty)
+
+    np.testing.assert_allclose(txi, tx_i, atol=8)
+    np.testing.assert_allclose(tyi, ty_i, atol=1)
+
+    tcxi, tcyi = d.__map_to_index__(tcx, tcy)
+
+    np.testing.assert_allclose(txi, tcxi, atol=8)
+    np.testing.assert_allclose(tyi, tcyi, atol=6)
+
+    vo = d.regrid(d.ds['sst'], t, pd.to_datetime("2022-05-06T02:00:00"), always_nearest=True)
     assert not np.isnan(vo.values).any()
 
