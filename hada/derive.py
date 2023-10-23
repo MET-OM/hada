3e148a736d8a55e7f8e362a5c5e7d03842997675
Gaute Hope <gauteh@met.no>
2023-10-23 12:50:11 +0200

    add horizontal visibilty dew point

--- hada/derive.py
+++ hada/derive.py
@@ -4,6 +4,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def hor_vis(rh, fog):
     """
     Calculate horizontal visibilty based on relative humidity and fog fraction.
@@ -11,7 +12,9 @@ def hor_vis(rh, fog):
     assert rh.shape == fog.shape, "rh and fog must be same dimensions"
 
     if not np.isfinite(rh).any() or not np.isfinite(fog).any():
-        logger.warning('horizontal visibilty: rh or fog completely nan filled, setting to NaN')
+        logger.warning(
+            'horizontal visibilty: rh or fog completely nan filled, setting to NaN'
+        )
         return xr.DataArray(np.full(rh.shape, np.nan), dims=rh.dims)
 
     if np.nanmax(fog) > 1.0 or np.nanmin(fog) < 0.0:
@@ -19,7 +22,6 @@ def hor_vis(rh, fog):
 
     # rh can be over-saturated, i.e.: more than 1.
     # assert rh.max() <= 1.0 and rh.min() >= 0.0, "relative humidity must be number between 0 and 1"
-
     """
     Calculates horizontal visibility based on a modified version of a formula proposed by Gultepe and Milbrandt (2010)
 
@@ -27,17 +29,39 @@ def hor_vis(rh, fog):
     """
 
     rh = rh * 100.
-    vv = (-5.19e-10)*(rh**5.44) + 40.1
+    vv = (-5.19e-10) * (rh**5.44) + 40.1
 
     ## Adjust for fog: set visibilty to zero when more than 90% fog.
-    vv.values[np.logical_and(fog>0.9, vv>1)] = 0.
+    vv.values[np.logical_and(fog > 0.9, vv > 1)] = 0.
 
     return vv
+
 
 def hor_vis_rh(rh):
     rh = rh * 100.
-    vv = (-5.19e-10)*(rh**5.44) + 40.1
+    vv = (-5.19e-10) * (rh**5.44) + 40.1
     return vv
+
+
+def hor_vis_tdta(dew_point_temp, air_temp):
+    """
+    Calculate visibilty based on dew point temp and air temp.
+
+    Temperatures in degrees Celsius.
+
+    Returns visibility in meters.
+    """
+    td = dew_point_temp
+    ta = air_temp
+
+    e = 6.11 * 10.**(7.5 * td / (237.5 + td))
+    es = 6.11 * 10.**(7.5 * ta / (237.5 + ta))
+    rh = 100. * e / es
+    vs = -0.000114 * rh**2.715 + 27.0
+    vs.values[vs < 0.05] = 0.05
+    vs = vs * 1.e3
+
+    return vs
 
 
 def derive(var, *args):
@@ -45,6 +69,8 @@ def derive(var, *args):
         return hor_vis(*args)
     elif var == 'horizontal_visibility_rh':
         return hor_vis_rh(*args)
+    elif var == 'horizontal_visibility_dew':
+        return hor_vis_tdta(*args)
     else:
         raise ValueError(f"Unknown derived variable: {var}")
 
--- tests/test_derive.py
+++ tests/test_derive.py
@@ -7,7 +7,7 @@ from hada.derive import *
 def test_horizontal_vis_meps():
     ds = xr.open_dataset("https://thredds.met.no/thredds/dodsC/mepslatest/meps_lagged_6_h_latest_2_5km_latest.nc")
 
-    ds = ds.sel(time='2022-12-07T00:00:00', method='nearest')
+    ds = ds.isel(time=0)
 
     rh = ds.relative_humidity_2m.isel(height0=0, ensemble_member=0).isel(x=slice(0, 10000), y=slice(0,10000))
     fog = ds.fog_area_fraction.isel(height1=0, ensemble_member=0).isel(x=slice(0, 10000), y=slice(0,10000))
@@ -16,3 +16,26 @@ def test_horizontal_vis_meps():
 
     vv = hor_vis(rh, fog)
     print(vv)
+
+def test_horizontal_vis_meps_tdta():
+    ds = xr.open_dataset("https://thredds.met.no/thredds/dodsC/mepslatest/meps_lagged_6_h_latest_2_5km_latest.nc")
+
+    ds = ds.isel(time=0)
+
+    rh = ds.relative_humidity_2m.isel(height0=0, ensemble_member=0).isel(x=slice(0, 10000), y=slice(0,10000))
+    fog = ds.fog_area_fraction.isel(height1=0, ensemble_member=0).isel(x=slice(0, 10000), y=slice(0,10000))
+
+    assert (fog>0.9).any()
+    vv = hor_vis(rh, fog)
+    print(vv)
+
+    ta = ds.air_temperature_2m.isel(height0=0, ensemble_member=0).isel(x=slice(0, 10000), y=slice(0,10000))
+    rh = ds.relative_humidity_2m.isel(height0=0, ensemble_member=0).isel(x=slice(0, 10000), y=slice(0,10000))
+    td = ta - ((100 - rh*100)/5.)
+
+    vv = hor_vis_tdta(td, ta)
+    print(vv)
+
+
+
+
